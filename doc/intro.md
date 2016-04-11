504d953f314ccdbc768cf89604bcf4347f147a5a
Sergio Alvarez-Napagao <tranchis@gmail.com>
2016-04-11 16:38:33 +0100

    Initial import

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to muon-schemas
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/muon_schemas/core_test.clj
@@ -0,0 +1,7 @@
+(ns muon-schemas.core-test
+  (:require [clojure.test :refer :all]
+            [muon-schemas.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
