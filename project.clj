b9c44de4cbcc72261f0a29b15d8319ce0f7dcf14
Sergio Alvarez-Napagao <tranchis@gmail.com>
2016-04-12 10:24:43 +0100

    Make caused-by a Long

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tranchis/muon-schemas "0.1.0"
+(defproject tranchis/muon-schemas "0.1.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
